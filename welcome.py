dca42614717ed655c86d1fad07d4319ffc0c2dde
Aeshah Alhabub <116434155+Aeshah20@users.noreply.github.com>
2024-04-16 20:39:45 +0300

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -1,2 +1,41 @@
-message= "Welcome to the world of programming!"
-print (message)
+Create a new folder called “software101
+On the window to the right,
+click on the File menu and select the “New Folder” option, as shown in the image below. Name the folder “software101”.
+
+ In software101, create a new file called “software.py”.
+Right-click on the folder software101, click on “New File”, create a new file, and name it “software.py”.
+
+Write code to add two arrays using Numpy library.
+
+Note: Since the library is already installed in the practice, there is no need to install it again.
+
+1- Import the numpy library, create two numpy arrays, and add them.
+2- import numpy as np
+
+a = np.array([2,3,4])
+b = np.array([3,2,1])
+c = a + b
+print (c)
+
+3. Run the program.
+cd software101
+python3.11 software.py
+
+4. Edit the software.py file and change one of the arrays.
+Change the array ‘a’ to [5,3,1] and save the file.
+
+5.Run the updated file.
+Run the program in the terminal using the below command:
+python3.11 software.py
+
+
+
+__________________________________________________________________________________
+# Author
+Pallavi Rai
+
+
+
+
+
+
